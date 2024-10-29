70cae9f2e4712d0cad7f02c8c3a49aa47e4a717c
algios <johanime1907@gmail.com>
2024-10-29 15:24:46 -0500

    finished v1 of cell class and moved state enum to a separate file

--- src/classes/cell.py
+++ src/classes/cell.py
@@ -1,9 +1,5 @@
 from __future__ import annotations
-from enum import IntEnum
-
-class EState(IntEnum):
-    DEAD = 0
-    ALIVE = 1
+from cell_state import EState
 
 class Cell:
     def __init__(self, state: EState, pos_x, pos_y) -> None:
@@ -16,7 +12,7 @@ class Cell:
 
     @state.setter
     def state(self, state: EState):
-        if(state in EState.__members__.values()):
+        if state in EState.__members__.values():
             self._state = state
 
     @property
@@ -30,29 +26,29 @@ class Cell:
     def calculate_state(self, *neighbors: Cell) -> EState:
         """
         Current rules:
-            - Any live cell with fewer than two live neighbors dies, as if 
+            - Any live cell with fewer than two live neighbors dies, as if
                 by underpopulation.
-            - Any live cell with two or three live neighbors lives on to the 
+            - Any live cell with two or three live neighbors lives on to the
                 next generation.
-            - Any live cell with more than three live neighbors dies, as if 
+            - Any live cell with more than three live neighbors dies, as if
                 by overpopulation.
-            - Any dead cell with exactly three live neighbors becomes a live 
+            - Any dead cell with exactly three live neighbors becomes a live
                 cell, as if by reproduction.
         """
         alive_neighbors = 0
         for neighbor in neighbors:
-            if (neighbor.state == EState.ALIVE):
+            if neighbor.state == EState.ALIVE:
                 alive_neighbors += 1
 
-        if (self._state == EState.ALIVE):
-            if (alive_neighbors == 0 or alive_neighbors == 1):
+        if self._state == EState.ALIVE:
+            if alive_neighbors == 0 or alive_neighbors == 1:
                 self._state = EState.DEAD
-            elif (alive_neighbors == 2 or alive_neighbors == 3):
+            elif alive_neighbors == 2 or alive_neighbors == 3:
                 self._state = EState.ALIVE
             else:
                 self._state = EState.DEAD
         else:
-            if (alive_neighbors == 3):
+            if alive_neighbors == 3:
                 self._state = EState.ALIVE
 
     def __str__(self):
